7d0cd09b14332ef08cd2ea6e9342f683ff9bcb0f
ky0nq <141110673+ky0nq@users.noreply.github.com>
2026-08-15 14:41:40 +0900

    UVM code upload

--- tb/apb_env.sv
+++ tb/apb_env.sv
@@ -1,19 +1,12 @@
 class apb_env extends uvm_env;
-
 	`uvm_component_utils(apb_env)
 
-	apb_master_agent master_agent;
-	apb_slave_agent  slave_agent;
-
+	apb_master_agent master_agt;
+	apb_slave_agent  slave_agt;
 	apb_sequencer    apb_sqr;
+	apb_scoreboard   scb;
 
-	apb_scoreboard   scoreboard;
-
-
-	function new(
-		string name = "apb_env",
-		uvm_component parent = null
-	);
+	function new(string name = "apb_env", uvm_component parent);
 		super.new(name, parent);
 	endfunction
 
@@ -21,66 +14,20 @@ class apb_env extends uvm_env;
 	function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
 
-
-		master_agent =
-			apb_master_agent::type_id::create(
-				"master_agent",
-				this
-			);
-
-
-		slave_agent =
-			apb_slave_agent::type_id::create(
-				"slave_agent",
-				this
-			);
-
-
-		apb_sqr =
-			apb_sequencer::type_id::create(
-				"apb_sqr",
-				this
-			);
-
-
-		scoreboard =
-			apb_scoreboard::type_id::create(
-				"scoreboard",
-				this
-			);
-
+		master_agt = apb_master_agent::type_id::create("master_agt", this);
+		slave_agt = apb_slave_agent::type_id::create("slave_agt", this);
+		apb_sqr = apb_sequencer::type_id::create("apb_sqr",	this);
+		scb = apb_scoreboard::type_id::create("scb", this);
 	endfunction
-
 
 	function void connect_phase(uvm_phase phase);
 		super.connect_phase(phase);
 
+		apb_sqr.master_sqr = master_agt.master_sqr;
+		apb_sqr.slave_sqr  = slave_agt.slave_sqr;;
 
-		// -----------------------------------------
-		// Virtual Sequencer
-		// -----------------------------------------
-		apb_sqr.apb_master_sqr =
-			master_agent.master_sqr;
-
-		apb_sqr.apb_slave_sqr =
-			slave_agent.slave_sqr;
-
-
-		// -----------------------------------------
-		// Master Monitor -> Scoreboard
-		// -----------------------------------------
-		master_agent.master_mon.master_ap.connect(
-			scoreboard.master_imp
-		);
-
-
-		// -----------------------------------------
-		// Slave Monitor -> Scoreboard
-		// -----------------------------------------
-		slave_agent.slave_mon.slave_ap.connect(
-			scoreboard.slave_imp
-		);
-
+		master_agt.master_mon.master_ap.connect(scb.master_imp);
+		slave_agt.slave_mon.slave_ap.connect(scb.slave_imp);
 	endfunction
 
 endclass
