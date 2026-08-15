8cc92e792f63ed2129c05841ac95eece2eee0423
ky0nq <141110673+ky0nq@users.noreply.github.com>
2026-08-15 15:09:44 +0900

    UVM code upload

--- tb/apb_test.sv
+++ tb/apb_test.sv
@@ -1,45 +1,39 @@
-class apb_base_test extends uvm_test;
-	`uvm_component_utils(apb_base_test)
-	apb_env env;
+import uvm_pkg::*;
+import apb_pkg::*;
 
-	function new(string name = "apb_base_test", uvm_component parent = null);
-		super.new(name, parent);
-	endfunction
+module tb_top;
 
-	function void build_phase(uvm_phase phase);
-		super.build_phase(phase);
-		env = apb_env::type_id::create("env", this);
-	endfunction
+	logic PCLK;
+	logic PRESETn;
 
-	function void connect_phase(uvm_phase phase);
-		super.connect_phase(phase);
-	endfunction
+	// clock generate
+	initial PCLK = 0;
+	always #5 PCLK = ~PCLK;
 
-	function void end_of_elaboration_phase(uvm_phase phase);
-		super.end_of_elaboration_phase(phase);
-		uvm_top.print_topology();
-	endfunction
-endclass
+	// interface instance
+	apb_if apb_vif (
+		.PCLK    (PCLK),
+		.PRESETn (PRESETn)
+	);
 
-class apb_basic_test extends apb_base_test;
-	`uvm_component_utils(apb_basic_test)
+	// ======================
+	// dut place
+	// ======================
+	
+	initial begin
+		PRESETn = 1'b0;
+		repeat(5) @(posedge PCLK);
+		PRESETn = 1'b1;
+	end
 
-	function new(string name="apb_basic_test", uvm_component parent);
-		super.new(name, parent);
-	endfunction
+	initial begin
+		uvm_config_db#(virtual apb_if)::set(null, "*", "apb_vif", apb_vif);
+		run_test("apb_test");
+	end
 
-	virtual task run_phase(uvm_phase phase);
-		apb_base_seq seq;
-
-		phase.raise_objection(this);
-		seq = apb_base_seq::type_id::create("seq");
-		if (!seq.randomize())
-			`uvm_error("TEST", "Seq Randomize Fail!!");
-		seq.num = 20;
-		seq.start(env.apb_sqr);
-
-		#100; // delay for stable state
-		phase.drop_objection(this);
-
-	endtask
-endclass
+	initial begin
+		$fsdbDumpfile("wave.fsdb");
+		$fsdbDumpvars(0);
+		$fsdbDumpMDA();
+	end
+endmodule
